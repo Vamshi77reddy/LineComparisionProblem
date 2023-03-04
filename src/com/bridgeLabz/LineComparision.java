4a355c8d83d1ff912658ba5b415412911fb8960c
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-04 11:32:53 +0530

    Added CheckEquality

--- src/com/bridgeLabz/LineComparision.java
+++ src/com/bridgeLabz/LineComparision.java
@@ -16,5 +16,25 @@ public static void main(String[ ] args) {
      int y2 = sc.nextInt();
      double length = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
      System.out.println("Length of Line between two points: " + length);
+     System.out.println("\nEnter points for second line:");
+     System.out.println("Enter point x3: ");
+     int x3 = sc.nextInt();
+     System.out.println("Enter point y3: ");
+     int y3 = sc.nextInt();
+     System.out.println("Enter point x4: ");
+     int x4 = sc.nextInt();
+     System.out.println("Enter point y4: ");
+     int y4 = sc.nextInt();
+     Double length2 = Math.sqrt((Math.pow((x4 - x3), 2)) + (Math.pow((y4 - y3), 2)));
+     System.out.println("Length of second line is: " + length2);
+
+     boolean result = length2.equals(length);
+
+     if (result == true) {
+         System.out.println("Lines are equal");
+     } else {
+         System.out.println("Lines are not equal");
+     }
+     
 }
 }
