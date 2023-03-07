460ca5444dbab41d6ef7dc3fac9ba3b7aa323080
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-07 22:16:20 +0530

    Added Refactor method

--- src/com/bridgeLabz/LineComparision.java
+++ src/com/bridgeLabz/LineComparision.java
@@ -3,40 +3,53 @@ package com.bridgeLabz;
 import java.util.Scanner;
 
 public class LineComparision {
-public static void main(String[ ] args) {
+	static int x1;
+	static int x2;
+	static int x3;
+	static int x4;
+	static int y1;
+	static int y2;
+	static int y3;
+	static int y4;
+	 static void checkEquality(){
+	     double length = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
+	     Double length2 = Math.sqrt((Math.pow((x4 - x3), 2)) + (Math.pow((y4 - y3), 2)));
+	     System.out.println("Length of second line is: " + length2);
+	     System.out.println("Length of Line between two points: " + length);
+	     int result = length2.compareTo(length);
+
+	     if (result == 0) {
+	         System.out.println("\nLines are equal");
+	     } else if (result > 0) {
+	         System.out.println("\nLine 2 is greater than line 1");
+	     } else {
+	         System.out.println("\nline 1 is greater than line 2");
+	     }
+
+		
+	}
+		public static void main(String[ ] args) {
+
 	System.out.println("Welcome to LineComparision");
 	 Scanner sc = new Scanner(System.in);
      System.out.println("Enter point x1: ");
-     int x1 = sc.nextInt();
+      x1 = sc.nextInt();
      System.out.println("Enter point y1: ");
-     int y1 = sc.nextInt();
+      y1 = sc.nextInt();
      System.out.println("Enter point x2: ");
-     int x2 = sc.nextInt();
+      x2 = sc.nextInt();
      System.out.println("Enter point y2: ");
-     int y2 = sc.nextInt();
-     double length = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-     System.out.println("Length of Line between two points: " + length);
+      y2 = sc.nextInt();
      System.out.println("\nEnter points for second line:");
      System.out.println("Enter point x3: ");
-     int x3 = sc.nextInt();
+      x3 = sc.nextInt();
      System.out.println("Enter point y3: ");
-     int y3 = sc.nextInt();
+      y3 = sc.nextInt();
      System.out.println("Enter point x4: ");
-     int x4 = sc.nextInt();
+      x4 = sc.nextInt();
      System.out.println("Enter point y4: ");
-     int y4 = sc.nextInt();
-     Double length2 = Math.sqrt((Math.pow((x4 - x3), 2)) + (Math.pow((y4 - y3), 2)));
-     System.out.println("Length of second line is: " + length2);
-
-     int result = length2.compareTo(length);
-
-     if (result == 0) {
-         System.out.println("\nLines are equal");
-     } else if (result > 0) {
-         System.out.println("\nLine 2 is greater than line 1");
-     } else {
-         System.out.println("\nline 1 is greater than line 2");
-     }
-     
+      y4 = sc.nextInt();
+   checkEquality();
+    
 }
 }
